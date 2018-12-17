64a2186aa69249aad1fa0e06c85c92bc13a5b25f
DwDan <dnlwillian@gmail.com>
2018-12-17 09:05:29 -0200

    Ajuste Tela Localizar

--- DwUniSys/UI/Localizar.Designer.cs
+++ DwUniSys/UI/Localizar.Designer.cs
@@ -70,6 +70,7 @@
             this.btn_Cancelar.TabIndex = 0;
             this.btn_Cancelar.Text = "Cancelar";
             this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
             // 
             // olvDados
             // 
